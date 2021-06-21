93509a15c7c9f199e6e4dbb8c12f174d7e699a14
Anas Mohamed <anaskamal659@gmail.com>
2021-06-21 11:15:03 -0700

    save

--- kafo/Presentaion/Customer/Main/View/Cell/MainViewTableViewCell.swift
+++ kafo/Presentaion/Customer/Main/View/Cell/MainViewTableViewCell.swift
@@ -15,6 +15,12 @@ class MainViewTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.backgroundColor = UIColor.init(red:68.0/255.0 , green: 68.0/255.0, blue:68.0/255.0 , alpha: 1.0)
+        self.companyNameLblView.backgroundColor = UIColor.init(red:68.0/255.0 , green: 68.0/255.0, blue:68.0/255.0 , alpha: 1.0)
+        companyNameLblView.layer.cornerRadius = 7
+        companyNameLblView.layer.borderColor = UIColor.init(red:187.0/255.0 , green: 180.0/255.0, blue:57.0/255.0 , alpha: 1.0).cgColor
+        
+        companyNameLblView.layer.borderWidth = 1
         containerView.layer.cornerRadius = 7
         containerView.layer.shadowColor = UIColor.gray.cgColor
         containerView.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
@@ -23,6 +29,8 @@ class MainViewTableViewCell: UITableViewCell {
              
              
         mainView.layer.cornerRadius = 7
+        mainView.layer.borderColor = UIColor.white.cgColor
+        mainView.layer.borderWidth = 1
         mainView.clipsToBounds = true
              
     }
--- kafo/Presentaion/Customer/Main/View/MainCustomerViewController.swift
+++ kafo/Presentaion/Customer/Main/View/MainCustomerViewController.swift
@@ -7,16 +7,33 @@
 
 import UIKit
 
-class MainCustomerViewController: UIViewController {
+class MainCustomerViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"MainViewTableViewCell" , for:indexPath)
+        return cell
+    }
+    
 
     @IBOutlet weak var mainCustomerServiceTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor.init(red:68.0/255.0 , green: 68.0/255.0, blue:68.0/255.0 , alpha: 1.0)
+        setupTableView()
+        mainCustomerServiceTableView.delegate = self
+        mainCustomerServiceTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    func setupTableView() {
+        mainCustomerServiceTableView.register(UINib(nibName: "MainViewTableViewCell", bundle: nil), forCellReuseIdentifier: "MainViewTableViewCell")
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
     /*
     // MARK: - Navigation
 
